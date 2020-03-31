bbc6a97b9579b701fd2d028f17583a006d000843
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-03-31 15:01:45 +0430

    [skip ci] update to the new model of pluf-orm

--- tests/conf/config.php
+++ tests/conf/config.php
@@ -1,64 +1,29 @@
 <?php
-// $cfg = include 'mysql.config.php';
-$cfg = include 'sqlite.config.php';
-
-$cfg['test'] = false;
-$cfg['timezone'] = 'Europe/Berlin';
-
-// Set the debug variable to true to force the recompilation of all
-// the templates each time during development
-$cfg['debug'] = true;
-$cfg['installed_apps'] = array(
-    'Pluf',
-    'User',
-    'Monitor',
-    'Tenant',
-    'SuperTms'
-);
-
-$cfg['multitenant'] = true;
 
 /*
- * Middlewares
+ * This file is part of Pluf Framework, a simple PHP Application Framework.
+ * Copyright (C) 2010-2020 Phoinex Scholars Co. http://dpq.co.ir
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-$cfg['middleware_classes'] = array(
-    // find tenant
-    'Pluf_Middleware_TenantEmpty',
-    'Pluf_Middleware_TenantFromHeader',
-    'Pluf_Middleware_TenantFromDomain',
-    'Pluf_Middleware_TenantFromSubDomain', // It should be used only in multitenant state
-    'Pluf_Middleware_TenantFromConfig',
-    // Sessions
-    'Pluf_Middleware_Session',
-    'User_Middleware_Session'
-);
+$cfg = array();
 
-$cfg['secret_key'] = '5a8d7e0f2aad8bdab8f6eef725412850';
+$cfg['db_engine'] = '\Pluf\Db\SQLiteEngine';
+$cfg['db_database'] = '/tmp/pluf.test.sqlite.db';
 
-// Temporary folder where the script is writing the compiled templates,
-// cached data and other temporary resources.
-// It must be writeable by your webserver instance.
-// It is mandatory if you are using the template system.
-$cfg['tmp_folder'] = __DIR__ . '/../tmp';
-$cfg['upload_path'] = __DIR__ . '/../storage/tenant';
-
-// The folder in which the templates of the application are located.
-$cfg['templates_folder'] = array(
-    __DIR__ . '/../templates'
-);
-
-/*
- * Template tags
- */
-$cfg['template_tags'] = array(
-    'config' => 'Pluf_Template_Tag_Mytag',
-    'setting' => 'Tenant_Template_Tag_Setting',
-    'config' => 'SuperTenant_Template_Tag'
-);
-
-// Default mimetype of the document your application is sending.
-// It can be overwritten for a given response if needed.
-$cfg['mimetype'] = 'text/html';
+$cfg['db_schema'] = '\Pluf\Db\SQLiteSchema';
+$cfg['db_schema_table_prefix'] = 'pluf_unit_tests_' . rand() . '_';
 
 return $cfg;
 
